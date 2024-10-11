0c67b59654dc004fbeaf498e2d5dff043f4e26cf
Jumballaya <nchupacabra@gmail.com>
2024-10-11 15:18:42 -0500

    Start working on a tower node floating menu

--- Assets/scripts/Node.cs
+++ Assets/scripts/Node.cs
@@ -23,36 +23,19 @@ public class Node : MonoBehaviour
     private Vector3 position;
     private Quaternion rotation;
 
-    private int lastElevation;
-    private NodeOrientation lastOrientation;
-
 
     void Start()
     {
-        lastElevation = elevation;
-        lastOrientation = orientiation;
         position = new Vector3(transform.position.x, elevation * 0.2f, transform.position.z);
         rotation = Quaternion.Euler(0f, 0f, GetOrientationDegrees());
         transform.SetPositionAndRotation(position, rotation);
     }
 
-    void FixedUpdate()
+    void OnValidate()
     {
-        bool updated = false;
-        if (elevation != lastElevation)
-        {
-            position.Set(transform.position.x, elevation * 0.2f, transform.position.z);
-            updated = true;
-        }
-        if (orientiation != lastOrientation)
-        {
-            rotation = Quaternion.Euler(0f, GetOrientationDegrees(), 0f);
-            updated = true;
-        }
-        if (updated)
-        {
-            transform.SetPositionAndRotation(position, rotation);
-        }
+        position.Set(transform.position.x, elevation * 0.2f, transform.position.z);
+        rotation = Quaternion.Euler(0f, GetOrientationDegrees(), 0f);
+        transform.SetPositionAndRotation(position, rotation);
     }
 
     private float GetOrientationDegrees()
