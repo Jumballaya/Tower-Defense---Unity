1df09b5b82113034d03e3040dfe733d261e851b3
Jumballaya <nchupacabra@gmail.com>
2024-10-17 14:54:19 -0500

    Fix issue with targeting + add some keyboard controls to move the camera

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
-
+        HandleMovement();
+        HandleScroll();
     }
+
+    private void HandleMovement()
+    {
+        Vector2 dir = new();
+        if (Input.GetKey(KeyCode.W))
+        {
+            dir.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            dir.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            dir.x += 1;
+        }
+        transform.position = new Vector3(transform.position.x + dir.x * 5f * Time.deltaTime, transform.position.y, transform.position.z + dir.y * 5f * Time.deltaTime);
+    }
+
+    private void HandleScroll() { }
 }
